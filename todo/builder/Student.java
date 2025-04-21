a71ee3c771678b4d9fefc9d0f37cf12269664feb
ductm2205 <ductranminh12342003@gmail.com>
2025-04-21 13:14:34 +0700

    builder

--- todo/builder/Student.java
+++ todo/builder/Student.java
@@ -9,38 +9,103 @@ public class Student {
 	private String phone;
 	private String address;
 
-	//TO-DO: Implement the constructor
+	// TO-DO: Implement the constructor
 	public Student(StudentBuilder studentBuilder) {
- 
+		this.id = studentBuilder.id;
+		this.age = studentBuilder.age;
+		this.firstName = studentBuilder.firstName;
+		this.lastName = studentBuilder.lastName;
+		this.address = studentBuilder.address;
+		this.phone = studentBuilder.phone;
 	}
 
-	//TO-DO: Implement GETTERs & SETTERs for given attributes
- 
-	//TO-DO: Implement toString() method to display student's information
+	// TO-DO: Implement GETTERs & SETTERs for given attributes
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	// TO-DO: Implement toString() method to display student's information
 	@Override
 	public String toString() {
-		return null; 
+		return "Student{id:" + this.id + ", firstname: " + this.firstName + ", lastname: " + this.lastName + ", age: "
+				+ this.age + ", phone: " + this.phone + ", address: " + this.address + "}";
 	}
-	
+
 	// Create the StudentBuilder inner class
 	public static class StudentBuilder {
-		//TO-DO: Declare the similar attributes with Student class
+		// TO-DO: Declare the similar attributes with Student class
+		private int id;
+		private String firstName;
+		private String lastName;
+		private int age;
+		private String phone;
+		private String address;
 
-		//TO-DO: Implement the constructor
+		// TO-DO: Implement the constructor
 		public StudentBuilder(int id, String firstName, String lastName) {
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
 
-		//TO-DO: Implement the 3 below methods
+		// TO-DO: Implement the 3 below methods
 		public StudentBuilder withOptionalAge(int age) {
-			return null;
+			this.age = age;
+			return this;
 		}
 
 		public StudentBuilder withOptionalPhone(String phone) {
-			return null;
+			this.phone = phone;
+			return this;
 		}
 
 		public StudentBuilder withOptionalAddress(String address) {
-			return null;
+			this.address = address;
+			return this;
 		}
 
 		public Student buildStudent() {
@@ -49,7 +114,6 @@ public class Student {
 		}
 
 		private boolean validateStudentData() {
-			// Validation process, check if student is registered in the database
 			return true;
 		}
 	}
